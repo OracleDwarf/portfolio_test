00b614b45b2309c39daa99dcd59206bed3743950
Edvard Burak <30567678+OracleDwarf@users.noreply.github.com>
2018-10-03 10:42:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const http = require('http');
 const fs = require('fs');
+const debug = require('debug');
 
-const server = http.createServer(function (request, response) {
-    console.log(request.method, request.url)
-    if (request.url === '/style.css'){
-        const css = fs.readFileSync('style.css' , 'utf8');
-        response.end(css);
-    } else {
-        const text = fs.readFileSync('index.html' , 'utf8');
-        response.end(text);
+const log = debug('*');
+
+const server = http.createServer((req, res) => {
+    const filename = req.url === '/' ? './public/index.html' : `./public/${req.url}`;
+
+log('request: %s', req.url);
+
+fs.readFile(filename, (err, file) => {
+    if (err) {
+        log('file %s not found', filename);
+
+        res.statusCode = 404;
+        res.write('404');
+        res.end();
+        return;
     }
+
+    log('file %s found', filename);
+res.write(file);
+res.end();
+});
 });
 
-console.log('port = ', process.env.PORT );
-
-
-server.listen(process.env.PORT || 3000);
-console.log('server started!');
+server.listen(3000, () => {
+    log('server started on port %d', 3000);
+});
